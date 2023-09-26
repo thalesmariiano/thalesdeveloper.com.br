6b0a2af4dd80e24ec0d87ba5711c3e65eb084c3a
Thales Mariano <thales.mariano125@gmail.com>
2023-09-26 17:20:57 -0300

    DrawerMenu styles done

--- src/components/DrawerMenu.vue
+++ src/components/DrawerMenu.vue
@@ -1,3 +1,14 @@
 <template>
-	
+	<div class="fixed top-[4.5rem] left-0 w-full h-full flex justify-end bg-neutral-900/75">
+		<div class="w-80 h-screen bg-myPurple-900 p-5 shadow-drawer">
+			<nav>
+				<ul class="text-right mt-2">
+					<li class="py-2"><a class="text-white text-2xl font-madeTommy hover:text-myPurple-100" href="#">Inicio</a></li>
+					<li class="py-2"><a class="text-white text-2xl font-madeTommy hover:text-myPurple-100" href="#">Sobre</a></li>
+					<li class="py-2"><a class="text-white text-2xl font-madeTommy hover:text-myPurple-100" href="#">Projetos</a></li>
+					<li class="py-2"><a class="text-white text-2xl font-madeTommy hover:text-myPurple-100" href="#">Contato</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
 </template>
