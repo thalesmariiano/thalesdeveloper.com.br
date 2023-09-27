183020f81dfb5c283390d51b6733cf8ea0531c56
Thales Mariano <thales.mariano125@gmail.com>
2023-09-27 14:10:28 -0300

    InicioSection component created

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
   <header>
     <AppHeader />
   </header>
+  <section>
+    <InicioSection />
+  </section>
 </template>
 
 <script setup>
   import AppHeader from '@/components/AppHeader.vue'
+  import InicioSection from '@/components/InicioSection.vue'
 </script>
 
 <style scoped>
---
+++ src/components/InicioSection.vue
@@ -0,0 +1,3 @@
+<template>
+	Inicio
+</template>
