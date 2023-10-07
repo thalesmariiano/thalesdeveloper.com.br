315f8fed2d30fad2f4e82741625ab1ccf40c285a
Thales Mariano <thales.mariano125@gmail.com>
2023-10-07 13:41:29 -0300

    ProjetosSection component created

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
   <AppHeader />
   <InicioSection />
   <SobreSection />
+  <ProjetosSection />
 </template>
 
 <script setup>
   import AppHeader from '@/components/AppHeader.vue'
   import InicioSection from '@/components/InicioSection.vue'
   import SobreSection from '@/components/SobreSection.vue'
+  import ProjetosSection from '@/components/ProjetosSection.vue'
 </script>
 
 <style scoped>
---
+++ src/components/ProjetosSection.vue
@@ -0,0 +1,5 @@
+<template>
+	<section>
+		Projetos		
+	</section>
+</template>
