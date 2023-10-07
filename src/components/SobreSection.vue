8156fc892dc16ebccf4c53af317eea51bec70028
Thales Mariano <thales.mariano125@gmail.com>
2023-10-07 00:28:30 -0300

    SobreSection done

--- src/components/SobreSection.vue
+++ src/components/SobreSection.vue
@@ -1,5 +1,45 @@
 <template>
 	<section>
-		Sobre Mim
+		<div class="w-full py-5 bg-myPurple-900 py-10 space-y-10">
+			<div class="w-full flex flex-col md:flex-row justify-center items-center gap-5">
+				<div class="w-72 shrink-0 h-96 bg-neutral-700 bg-[url('./assets/me.jpg')] bg-center bg-cover rounded">
+					
+				</div>
+				<div class="text-center md:text-left space-y-2">
+					<h1 class="text-xl text-myPurple-100 font-madeTommy">Sobre Mim</h1>
+					<div class="w-80 sm:w-3/5 md:w-[28rem] mx-auto md:mx-0 space-y-4 px-3 sm:px-0">
+						<p class="font-madeTommy text-white">
+							Meu nome é Thales, Mariano tenho 20 anos e sou um
+							Web Developer com 1 ano de experiência profissionalmente desenvolvendo sites e aplicações web. Minha paixão pela criação de sites me levou
+							a buscar conhecimentos sólidos em diversas tecnologias
+							e frameworks.
+						</p>
+						<p class="font-madeTommy text-white">
+							Possuo um conhecimento sólido em tecnologias
+							de front-end, como VueJs, TailwindCss, JavaScript e
+							Php. Essa combinação de habilidades me
+							permite criar diariamente aplicações web atrativas,
+							fáceis de usar e que atendem às necessidades
+							específicas de cada projeto. Utilizo essas
+							ferramentas de forma eficiente, garantindo uma
+							experiência de usuário excepcional e uma interface
+							visualmente agradável.
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="text-center space-y-4 mt-2 px-3">
+				<h1 class="text-xl text-myPurple-100 font-madeTommy">Habilidades</h1>
+				<div class="w-full flex gap-3 flex-wrap justify-center items-center">
+					<img src="https://skillicons.dev/icons?i=vuejs" alt="VueJs" />
+					<img src="https://skillicons.dev/icons?i=tailwindcss" alt="TalwindCSS" />
+					<img src="https://skillicons.dev/icons?i=html" alt="HTML" />
+					<img src="https://skillicons.dev/icons?i=css" alt="CSS"/>
+					<img src="https://skillicons.dev/icons?i=javascript" alt="JavaScript" />
+					<img src="https://skillicons.dev/icons?i=laravel" alt="Laravel" />
+					<img src="https://skillicons.dev/icons?i=mysql" alt="MySQL" />
+				</div>
+			</div>
+		</div>
 	</section>
 </template>
