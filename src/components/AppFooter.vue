ce1fd22b75c396ddfa37042dcf847782f921d87b
Thales Mariano <thales.mariano125@gmail.com>
2023-10-08 16:10:26 -0300

    AppFooter styles done

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,26 @@
 <template>
 	<footer>
-		Footer
+		<div class="w-full bg-myPurple-400 px-10 lg:px-24 py-5 flex flex-col md:flex-row justify-center md:justify-between items-center">
+			<div class="hidden md:block">
+				<img class="w-40" src="../assets/logo_white.png">
+			</div>
+			<div class="text-center">
+				<p class="text-white text-xs font-madeTommy">
+					&copy 2023 <span class="font-bold">Thales Web Developer</span>. Todos os diretos reservados.
+				</p>
+				<p class="text-white text-xs font-madeTommy">
+					Design feito por <span class="font-bold">IV Studio</span>.
+				</p>
+			</div>
+			<div class="hidden md:block">
+				<img class="w-40" src="../assets/ivstudio_logo.png">
+				
+			</div>
+			<div class="md:hidden flex justify-center items-center  gap-3 mt-3">
+				<img class="w-32" src="../assets/logo_white.png">
+				<span class="w-0.5 h-8 bg-white rounded"></span>
+				<img class="w-32" src="../assets/ivstudio_logo.png">
+			</div>
+		</div>
 	</footer>
 </template>
