8e4ab0b4e1e802f467d175222e7ae76d6feb024f
Thales Mariano <thales.mariano125@gmail.com>
2023-10-08 15:33:04 -0300

    ContatoSection done

--- src/components/ContatoSection.vue
+++ src/components/ContatoSection.vue
@@ -1,5 +1,12 @@
 <template>
 	<section>
-		Contato
+		<div class="w-full text-center bg-myPurple-800 py-10 space-y-10r">
+			<h1 class="font-kusanagi text-white text-shadow text-4xl sm:text-5xl">Contato</h1>
+			<ContatoForm />
+		</div>
 	</section>
 </template>
+
+<script setup>
+	import ContatoForm from './ContatoForm.vue'
+</script>
