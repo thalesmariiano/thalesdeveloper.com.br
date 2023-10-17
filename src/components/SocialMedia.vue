77c186c73acf6a0be2efabefd5adf289b4879ace
Thales Mariano <thales.mariano125@gmail.com>
2023-10-17 16:13:48 -0300

    SocialMedia component done

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,3 +1,16 @@
 <template>
-	social media
+	<div class="w-full flex justify-center items-center gap-x-5 pt-10">
+		<div class="w-9 h-9 hover:-translate-y-2 transition mt-1">
+			<a href="https://www.twitter.com/dev_thales" target="_blank"><img src="../assets/twitter-mark-white.svg" alt="twitter logo"></a>
+		</div>
+		<div class="w-9 h-9 hover:-translate-y-2 transition">
+			<a href="https://www.instagram.com/dev.thales/" target="_blank"><img src="../assets/instagram-mark-white.svg" alt="instagram logo"></a>
+		</div>
+		<div class="w-9 h-9 hover:-translate-y-2 transition">
+			<a href="https://github.com/thalesmariiano" target="_blank"><img src="../assets/github-mark-white.svg" alt="github logo"></a>
+		</div>
+		<div class="w-9 h-9 hover:-translate-y-2 transition">
+			<a href="https://tiktok.com/@dev.thales" target="_blank"><img src="../assets/tiktok-mark-white.svg" alt="tiktok logo"></a>
+		</div>
+	</div>
 </template>
