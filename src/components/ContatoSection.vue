8723d589f2e2726bcb8f1d3f327bb36b109cc1ae
Thales Mariano <thales.mariano125@gmail.com>
2024-07-12 11:25:08 -0300

    ContatoSection updated

--- src/components/ContatoSection.vue
+++ src/components/ContatoSection.vue
@@ -1,14 +1,23 @@
 <template>
 	<section id="contato">
-		<div class="w-full text-center bg-myPurple-800 py-10 space-y-10r">
+		<div class="w-full text-center bg-myPurple-800 py-10">
 			<h1 class="font-kusanagi text-white text-shadow text-4xl sm:text-5xl">Contato</h1>
-			<ContatoForm />
+
 			<SocialMedia />
+
+			<div class="w-full flex flex-col justify-center items-center">
+				<p class="font-madeTommy text-neutral-400 mb-2 text-sm">Ou mande um email:</p>
+				<a class="flex items-center gap-x-3 px-5 py-1 text-lg font-madeTommy text-neutral-300 bg-myPurple-800 rounded border border-myPurple-400 hover:bg-myPurple-400 shadow-button transition-all cursor-pointer"
+				   href="mailto: thaleswebdev@gmail.com"
+				>
+					<img class="w-4" src="@/assets/icons/envelope-fill-white.svg">
+					thaleswebdev@gmail.com
+				</a>
+			</div>
 		</div>
 	</section>
 </template>
 
 <script setup>
  	import SocialMedia from './SocialMedia.vue'
-	import ContatoForm from './ContatoForm.vue'
 </script>
--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="w-full flex justify-center items-center gap-x-5 pt-10">
+	<div class="w-full flex justify-center items-center gap-x-5 mt-12 pb-10">
 		<div class="w-9 h-9 hover:-translate-y-2 transition mt-1">
-			<a href="https://www.twitter.com/dev_thales" target="_blank"><img src="../assets/twitter-mark-white.svg" alt="twitter logo"></a>
+			<a href="https://www.x.com/dev_thales" target="_blank"><img class="w-8 h-8" src="@/assets/icons/x-mark-white.svg" alt="x (twitter) logo"></a>
 		</div>
 		<div class="w-9 h-9 hover:-translate-y-2 transition">
-			<a href="https://www.instagram.com/dev.thales/" target="_blank"><img src="../assets/instagram-mark-white.svg" alt="instagram logo"></a>
+			<a href="https://www.instagram.com/dev.thales/" target="_blank"><img src="@/assets/icons/instagram-mark-white.svg" alt="instagram logo"></a>
 		</div>
 		<div class="w-9 h-9 hover:-translate-y-2 transition">
-			<a href="https://github.com/thalesmariiano" target="_blank"><img src="../assets/github-mark-white.svg" alt="github logo"></a>
+			<a href="https://github.com/thalesmariiano" target="_blank"><img src="@/assets/icons/github-mark-white.svg" alt="github logo"></a>
 		</div>
 		<div class="w-9 h-9 hover:-translate-y-2 transition">
-			<a href="https://tiktok.com/@dev.thales" target="_blank"><img src="../assets/tiktok-mark-white.svg" alt="tiktok logo"></a>
+			<a href="https://tiktok.com/@dev.thales" target="_blank"><img src="@/assets/icons/tiktok-mark-white.svg" alt="tiktok logo"></a>
 		</div>
 	</div>
 </template>
