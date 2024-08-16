75e88a1aa322a861088b767c609cdd8f71968ac0
Thales Mariano <thales.mariano125@gmail.com>
2024-08-15 22:23:58 -0300

    props import updated and fixes

--- src/components/ProjetoCard.vue
+++ src/components/ProjetoCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div :class="cardClass">
+	<div :class="card_class">
 		<div class="w-full">
 			<img class="mx-auto w-72 md:w-80" :src="project.image" :alt="project.name">
 		</div>
@@ -20,55 +20,38 @@
 				</p>
 			</div>
 			<div class="space-x-3 mt-2">
-				<a :class="linkClass" :href="siteLink" target="_blank">Acessar</a>
-				<a :class="repositoryClass" :href="repositoryLink" target="_blank">Github</a>
+				<a :class="link_class" :href="project.link" target="_blank">Acessar</a>
+				<a :class="repository_class" :href="project.repository" target="_blank">Github</a>
 			</div>
 		</div>
 	</div>
 </template>
 
-<script>
-	export default {
-		props: {
-			project: {
-				type: Object,
-				default: {}
-			}
-		},
-		setup(props){
-			const cardClass = props.project.id % 2 ? 'card-normal' : 'card-reverse'
+<script setup>
+	const { project } = defineProps({project: null})
 
-			const siteLink = props.project.link
-			const repositoryLink = props.project.repository
-
-			const linkClass = props.project.link ? 'project-button-active' : 'project-button-desactived' 
-			const repositoryClass = props.project.repository ? 'project-button-active' : 'project-button-desactived' 
-
-
-			return {
-				siteLink,
-				repositoryLink,
-				linkClass,
-				repositoryClass,
-				cardClass
-			}
-		}
-	}
+	const card_class = project.id % 2 ? 'card card-normal' : 'card card-reverse'
+	const link_class = project.link ? 'project-button' : 'project-button-desactived' 
+	const repository_class = project.repository ? 'project-button' : 'project-button-desactived' 
 </script>
 
 <style scoped>
-	.project-button-active {
+	.project-button {
 		@apply px-5 py-1 text-lg font-madeTommy text-neutral-300 bg-myPurple-900 rounded border border-myPurple-400 hover:bg-myPurple-400 hover:text-myPurple-900 transition-all cursor-pointer
 	}
 
 	.project-button-desactived {
 		@apply px-5 py-1 text-lg font-madeTommy text-neutral-300 bg-myPurple-900 rounded border border-myPurple-400 cursor-not-allowed opacity-50
 	}
-	.card-normal {
-		@apply w-full max-w-xs sm:max-w-sm md:max-w-3xl p-5 bg-myPurple-900/40 flex flex-col md:flex-row justify-between items-center rounded gap-y-4 border border-myPurple-400
-	}
-	.card-reverse {
-		@apply w-full max-w-xs sm:max-w-sm md:max-w-3xl px-12 py-5 bg-myPurple-900/40 flex flex-col md:flex-row-reverse justify-between items-center rounded gap-y-4 border border-myPurple-400
+
+	.card {
+		@apply w-full max-w-xs sm:max-w-sm md:max-w-3xl flex flex-col justify-between items-center bg-myPurple-900/40 rounded gap-y-4 border border-myPurple-400
 	}
 
+	.card-normal {
+		@apply p-5 md:flex-row
+	}
+	.card-reverse {
+		@apply px-12 py-5 md:flex-row-reverse
+	}
 </style>
