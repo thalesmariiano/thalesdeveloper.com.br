d83d68032865bd6e5c96c213b8d5a054ba95bc63
Thales Mariano <thales.mariano125@gmail.com>
2024-09-20 23:34:30 -0300

    [Update] - New social media added

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -1,16 +1,18 @@
 <template>
-	<div class="w-full flex justify-center items-center gap-x-5 mt-12 pb-10">
+	<div class="w-full flex justify-center items-center gap-x-5">
 		<div class="w-9 h-9 hover:-translate-y-2 transition mt-1">
-			<a href="https://www.x.com/dev_thales" target="_blank"><img class="w-8 h-8" src="@/assets/icons/x-mark-white.svg" alt="x (twitter) logo"></a>
+			<a href="https://bsky.app/profile/thalesdeveloper.com.br" target="_blank">
+				<svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" alt="Bluesky logo" fill="#fff" viewBox="0 0 568 501"><title>Bluesky</title><path fill="#fff" d="M123.121 33.664C188.241 82.553 258.281 181.68 284 234.873c25.719-53.192 95.759-152.32 160.879-201.21C491.866-1.611 568-28.906 568 57.947c0 17.346-9.945 145.713-15.778 166.555-20.275 72.453-94.155 90.933-159.875 79.748C507.222 323.8 536.444 388.56 473.333 453.32c-119.86 122.992-172.272-30.859-185.702-70.281-2.462-7.227-3.614-10.608-3.631-7.733-.017-2.875-1.169.506-3.631 7.733-13.43 39.422-65.842 193.273-185.702 70.281-63.111-64.76-33.89-129.52 80.986-149.071-65.72 11.185-139.6-7.295-159.875-79.748C9.945 203.659 0 75.291 0 57.946 0-28.906 76.135-1.612 123.121 33.664Z"></path></svg>
+			</a>
 		</div>
-		<div class="w-9 h-9 hover:-translate-y-2 transition">
-			<a href="https://www.instagram.com/dev.thales/" target="_blank"><img src="@/assets/icons/instagram-mark-white.svg" alt="instagram logo"></a>
+		<div class="w-8 h-8 hover:-translate-y-2 transition">
+			<a href="https://www.instagram.com/dev.thales/" target="_blank"><img src="@/assets/icons/instagram-mark-white.svg" title="Instagram" alt="Instagram logo"></a>
 		</div>
-		<div class="w-9 h-9 hover:-translate-y-2 transition">
-			<a href="https://github.com/thalesmariiano" target="_blank"><img src="@/assets/icons/github-mark-white.svg" alt="github logo"></a>
+		<div class="w-8 h-8 hover:-translate-y-2 transition">
+			<a href="https://github.com/thalesmariiano" target="_blank"><img src="@/assets/icons/github-mark-white.svg" title="Github" alt="Github logo"></a>
 		</div>
-		<div class="w-9 h-9 hover:-translate-y-2 transition">
-			<a href="https://tiktok.com/@dev.thales" target="_blank"><img src="@/assets/icons/tiktok-mark-white.svg" alt="tiktok logo"></a>
+		<div class="w-8 h-8 hover:-translate-y-2 transition">
+			<a href="https://tiktok.com/@dev.thales" target="_blank"><img src="@/assets/icons/tiktok-mark-white.svg" title="Tiktok" alt="Tiktok logo"></a>
 		</div>
 	</div>
 </template>
