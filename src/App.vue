8c45f398a29185baa7128f77e2f2703797950ef9
Thales Mariano <thales.mariano125@gmail.com>
2024-09-20 23:33:30 -0300

    [Update] - Site in standby mode

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
-  <AppHeader />
-  <InicioSection />
-  <SobreSection />
-  <ProjetosSection />
-  <ContatoSection />
-  <AppFooter />
+
+  <div class="w-full h-screen reative flex flex-col gap-y-12 justify-center items-center bg-gradient-to-tr from-myPurple-700 to-myPurple-100">
+      <img class="w-64 sm:w-80" src="@/assets/logo_purple_white.png">
+
+      <div class="absolute bottom-10">
+        <p class="text-white/60 font-madeTommy ml-2 mb-5">Site em manutenção! Voltamos em breve.</p>
+        <SocialMedia class="opacity-60"/>
+      </div>
+      
+  </div>
+
 </template>
 
 <script setup>
-  import AppHeader from '@/components/AppHeader.vue'
-  import InicioSection from '@/components/InicioSection.vue'
-  import SobreSection from '@/components/SobreSection.vue'
-  import ProjetosSection from '@/components/ProjetosSection.vue'
-  import ContatoSection from '@/components/ContatoSection.vue'
-  import AppFooter from '@/components/AppFooter.vue'
+  import SocialMedia from '@/components/SocialMedia.vue'
+  
 </script>
 
 <style scoped>
